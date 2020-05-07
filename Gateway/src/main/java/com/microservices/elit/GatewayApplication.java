6617bb5387b5b3c84a827cb10a507f30fb623577
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2020-05-07 02:02:47 +0200

    khesso ui berk

--- Gateway/src/main/java/com/microservices/elit/GatewayApplication.java
+++ Gateway/src/main/java/com/microservices/elit/GatewayApplication.java
@@ -38,7 +38,23 @@ public class GatewayApplication extends WebSecurityConfigurerAdapter {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 	
-
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 	
 	@Bean
 	@LoadBalanced
